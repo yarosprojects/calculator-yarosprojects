e7ce66f38900a789f6e72b984425526ee7ee631b
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-14 19:48:14 +0100

    Final stage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [CalculatorComponent],
+  imports: [CalculatorComponent, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
