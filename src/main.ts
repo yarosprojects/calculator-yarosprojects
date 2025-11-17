0cd02d8956cf6cfc793635e03a22f53643a43a45
Yaroslav Klymko <yaros.trabajo22@gmail.com>
2025-11-17 01:42:21 +0100

    Trying to add Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { inject } from '@vercel/analytics';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
+  .then(() => inject())
   .catch((err) => console.error(err));
